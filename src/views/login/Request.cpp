739e37147533f23caf0da0558f7c970e5e444805
sabudilovskiy <sabudilovskiy@yandex.ru>
2023-03-30 22:39:18 +0000

    -

--- src/views/login/Request.cpp
+++ src/views/login/Request.cpp
@@ -6,18 +6,10 @@
 #include "userver/server/http/http_request.hpp"
 #include "userver/formats/json.hpp"
 
-struct Test{
-    std::string id;
-};
-
 
 namespace timetable_vsu_backend::views::login{
     Request Parse(const userver::server::http::HttpRequest& value, userver::formats::parse::To<Request>){
         auto body = userver::formats::json::FromString(value.RequestBody());
-        Test test{
-            .id = "sdsd"
-        };
-        LOG_DEBUG() << test.id;
         return Request {
             body["login"].As<std::string>(), 
             body["password"].As<std::string>()
