8a6f0ecb829d1dd18b7e7e44e428253e8fb54c4a
sabudilovskiy <sabudilovskiy@yandex.ru>
2023-03-31 22:32:24 +0000

    -

--- tests/login/test_login_basic.py
+++ tests/login/test_login_basic.py
@@ -2,6 +2,7 @@ import pytest
 
 from testsuite.databases import pgsql
 
+
 @pytest.mark.pgsql('db_1', files=['initial_data_auth.sql'])
 async def test_login_endpoint(service_client):
     response = await service_client.post(
@@ -12,15 +13,6 @@ async def test_login_endpoint(service_client):
     assert 'token' in response.json()
     assert isinstance(response.json()['token'], str)
 
-@pytest.mark.pgsql('db_1', files=['initial_data_auth.sql'])
-async def test_login_endpoint_success(service_client):
-    response = await service_client.post(
-        '/login',
-        json={'login': 'test_user', 'password': 'password123'}
-    )
-    assert response.status == 200
-    assert 'token' in response.json()
-    assert isinstance(response.json()['token'], str)
 
 @pytest.mark.pgsql('db_1', files=['initial_data_auth.sql'])
 async def test_login_endpoint_success(service_client):
@@ -31,6 +23,18 @@ async def test_login_endpoint_success(service_client):
     assert response.status == 200
     assert 'token' in response.json()
     assert isinstance(response.json()['token'], str)
+
+
+@pytest.mark.pgsql('db_1', files=['initial_data_auth.sql'])
+async def test_login_endpoint_success(service_client):
+    response = await service_client.post(
+        '/login',
+        json={'login': 'test_user', 'password': 'password123'}
+    )
+    assert response.status == 200
+    assert 'token' in response.json()
+    assert isinstance(response.json()['token'], str)
+
 
 @pytest.mark.pgsql('db_1', files=['initial_data_auth.sql'])
 async def test_login_endpoint_wrong_password(service_client):
@@ -40,12 +44,14 @@ async def test_login_endpoint_wrong_password(service_client):
     )
     assert response.status == 401
 
+
 async def test_login_endpoint_missing_username(service_client):
     response = await service_client.post(
         '/login',
         json={'password': 'password123'}
     )
     assert response.status == 400
+
 
 @pytest.mark.pgsql('db_1', files=['initial_data_auth.sql'])
 async def test_login_endpoint_missing_password(service_client):
@@ -54,4 +60,3 @@ async def test_login_endpoint_missing_password(service_client):
         json={'login': 'test_user'}
     )
     assert response.status == 400
-
